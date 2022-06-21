30996698deab2082417be2e38c3a11df48782e42
Prabandh <prabandhgowda4@gmail.com>
2022-06-21 15:45:00 +0000

    pushing the file to empty repo

---
+++ condi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Enter the first number to compare"
+read num1
+echo "Enter the second number to compare"
+read num2
+
+if [ $num1 -gt $num2 ]
+then
+	echo "$num1 is greater than $num2"
+elif [ $num2 -gt $num1 ]
+then 
+	echo "$num2 is greater than $num1"	
+
+else
+	echo "$num2 is equal to $num1"
+fi
+
+
+
